5630b7728622a6717b5c9f387c1ee435183bdefc
SingWang123 <gnissingtest@gmail.com>
2024-07-27 01:59:12 +0800

    test4

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
 
-const HomePage: React.FC = () => {
+export default function HomePage() {
   return (
-    <div className = "index">
-      <div className = 'index__title'>
+    <div className="index">
+      <div className="index__title">
         React練習專案
       </div>
-      <div className = "index__content">
+      <div className="index__content">
         歡迎光臨我的頁面
       </div>
-      <div className = "index__button_bg">
-        <Link href = "/accounting">
-          <button style = {{width : "80px", height : "30px", fontSize : "16px"}}>點此開始</button>
+      <div className="index__button_bg">
+        <Link href="/accounting">
+          <button style={{ width: "80px", height: "30px", fontSize: "16px" }}>點此開始</button>
         </Link>
       </div>
     </div>
   );
-};
-
-export default HomePage;
+}
