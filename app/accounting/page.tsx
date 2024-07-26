053a5bfa30ab2984ffd491ca1940f4b95efe5dd4
SingWang123 <gnissingtest@gmail.com>
2024-07-27 01:31:33 +0800

    test2

---
+++ app/accounting/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Form } from '../components/Form';
+import { List } from '../components/List';
+
+export default function AccountingPage() {
+  return (
+    <div className = "index">
+        <Form />
+        <List />
+        happy happy
+    </div>
+  );
+};
